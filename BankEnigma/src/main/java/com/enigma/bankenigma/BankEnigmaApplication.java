b57ad69b2aff80503c1242ed3c7e51ddd7ddb59d
anugrah <gambanggambangsusah@gmail.com>
2022-01-05 23:00:44 +0700

    [Module] Add Bank Enigma Module

---
+++ BankEnigma/src/main/java/com/enigma/bankenigma/BankEnigmaApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.bankenigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEnigmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankEnigmaApplication.class, args);
+    }
+
+}
---
+++ BankEnigma/src/test/java/com/enigma/bankenigma/BankEnigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.bankenigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEnigmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
